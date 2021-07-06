216adccaa6b27c472725d194e256b493c0aa4c3c
Pepsinside <pepsinside@msn.com>
2021-07-07 01:51:18 +0200

    checkpoints

--- src/util.cpp
+++ src/util.cpp
@@ -1229,6 +1229,8 @@ void ReadConfigFile(map<string, string>& mapSettingsRet,
                fprintf(ConfFile, "rpcallowip=127.0.0.1\n");
                fprintf(ConfFile, "addnode=167.99.240.96:19427\n");
                fprintf(ConfFile, "addnode=167.99.240.96\n");
+               fprintf(ConfFile, "addnode=46.101.247.122:19427\n");
+               fprintf(ConfFile, "addnode=46.101.247.122\n");
                /*fprintf(ConfFile, "addnode=143.198.130.185:19427\n");
                fprintf(ConfFile, "addnode=143.198.130.185\n");
                fprintf(ConfFile, "addnode=45.77.210.8:19427\n");
